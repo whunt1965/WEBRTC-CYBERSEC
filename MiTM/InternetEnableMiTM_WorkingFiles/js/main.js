f3a223820d6e8c7971bc77f97eaa276df18b2fc7
Wiley Hunt <johnhunt@MacBook-Pro.local>
2020-11-05 15:35:38 -0500

    Reverted stun list after testing

--- MiTM/InternetEnableMiTM_WorkingFiles/js/main.js
+++ MiTM/InternetEnableMiTM_WorkingFiles/js/main.js
@@ -18,10 +18,15 @@ var tellA_toCall = false;
 var BremoteStream;
 
 const configuration = {
-  'iceServers': [
-      {'urls': 'stun:stun.l.google.com:19302'},
-      {"urls":"turn:numb.viagenie.ca", "username":"webrtc@live.com", "credential":"muazkh"}
+  iceServers: [
+    {
+      urls: [
+        'stun:stun1.l.google.com:19302',
+        'stun:stun2.l.google.com:19302',
+      ],
+    },
   ],
+  iceCandidatePoolSize: 10,
 };
 
 // Set up audio and video regardless of what devices are present.
